c5a1fe2dafcb23a34e25e0148899a78cdaab6967
Abdiel Beckford <abdielrodriguezbeckford@gmail.com>
2024-05-09 15:46:26 -0500

    push

--- features/tests/Target_signin.py
+++ features/tests/Target_signin.py
@@ -3,19 +3,31 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from time import sleep
+from behave import given, when, then
+
+driver_path = ChromeDriverManager().install()
+
+# create a new Chrome browser instance
+service = Service(driver_path)
+driver = webdriver.Chrome(service=service)
+driver.maximize_window()
+
 
 # open the url
 @given('Open Target main page')
 def open_target_main_page(context):
     context.driver.get('https://www.target.com/')
 
+
 @when('Click Target signin button')
 def click_target_signin_button(context):
     context.driver.find_element(By.CSS_SELECTOR, "a[aria-label='Account, sign in']").click()
 
+
 @when("Click 'Sign in' as the pop-up appears")
 def click_target_signin_popup(context):
     context.driver.find_element(By.CSS_SELECTOR, "a[data-test*=signIn]")
+
 
 @then("Verify your taken to 'Sign into your Target account' page")
 def verify_target_signin_page(context):
